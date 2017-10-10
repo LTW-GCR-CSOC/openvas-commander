c31b42388e5edbd84d615cf41179247ae8cd1624
Brandon Hurley <brandonhurley2011@gmail.com>
2017-10-10 06:14:13 -0700

    Removed Redis install and config function
    Redis will be installed and configured separately from the other dependencies.

--- openvas_commander.sh
+++ openvas_commander.sh
@@ -2,16 +2,7 @@
 
 function install_dependencies()
 {
-    apt-get install -y build-essential devscripts dpatch curl libassuan-dev libglib2.0-dev libgpgme11-dev libpcre3-dev libpth-dev libwrap0-dev libgmp-dev libgmp3-dev libgpgme11-dev libpcre3-dev libpth-dev quilt cmake pkg-config libssh-dev libglib2.0-dev libpcap-dev libgpgme11-dev uuid-dev bison libksba-dev doxygen sqlfairy xmltoman sqlite3 libsqlite3-dev wamerican redis-server libhiredis-dev libsnmp-dev libmicrohttpd-dev libxml2-dev libxslt1-dev xsltproc libssh2-1-dev libldap2-dev autoconf nmap libgnutls28-dev gnutls-bin libpopt-dev heimdal-dev heimdal-multidev libpopt-dev mingw32 texlive-full rpm alien nsis rsync python2.7 python-setuptools checkinstall
-}
-
-function configure_redis()
-{
-    if [ ! -f /etc/redis/redis.orig ]; then
-        cp /etc/redis/redis.conf /etc/redis/redis.orig
-        echo "unixsocket /tmp/redis.sock" >> /etc/redis/redis.conf
-        service redis-server restart        
-    fi
+    apt-get install -y build-essential devscripts dpatch curl libassuan-dev libglib2.0-dev libgpgme11-dev libpcre3-dev libpth-dev libwrap0-dev libgmp-dev libgmp3-dev libgpgme11-dev libpcre3-dev libpth-dev quilt cmake pkg-config libssh-dev libglib2.0-dev libpcap-dev libgpgme11-dev uuid-dev bison libksba-dev doxygen sqlfairy xmltoman sqlite3 libsqlite3-dev wamerican libhiredis-dev libsnmp-dev libmicrohttpd-dev libxml2-dev libxslt1-dev xsltproc libssh2-1-dev libldap2-dev autoconf nmap libgnutls28-dev gnutls-bin libpopt-dev heimdal-dev heimdal-multidev libpopt-dev mingw32 texlive-full rpm alien nsis rsync python2.7 python-setuptools checkinstall
 }
 
 function get_openvas_source_table()
@@ -191,7 +182,6 @@ elif [ "$1" == "--configure-all" ]
 then
     mkdir /usr/local/var/lib/openvas/openvasmd/
     mkdir /usr/local/var/lib/openvas/openvasmd/gnupg
-    configure_redis
     mkcerts
     ldconfig
     openvasmd --create-user=admin --role=Admin && openvasmd --user=admin --new-password=1
